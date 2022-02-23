8887e9a2978977ce47a17ae6254d15b0319e50e8
Denis <denis.vishchak@gmail.com>
2022-02-23 23:28:00 +0200

    Initial commit;

---
+++ src/main/java/com/home/homebookkeeping/HomeBookkeepingApplication.java
@@ -0,0 +1,13 @@
+package com.home.homebookkeeping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeBookkeepingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeBookkeepingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/home/homebookkeeping/HomeBookkeepingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.homebookkeeping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeBookkeepingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
